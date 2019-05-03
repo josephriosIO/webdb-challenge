b80d204105e34b6ca6c4b58a41ed0816d6fdc1c6
josephriosio <blankworldwide@gmail.com>
2019-05-03 08:24:03 -0700

    added endpoints to complete mvp

---
+++ data/helpers/projectModels.js
@@ -0,0 +1,35 @@
+const db = require("../dbConfig.js");
+const actionDb = require("./actionModels");
+
+function insert(project) {
+  return db("projects").insert(project);
+}
+
+function get() {
+  return db("projects");
+}
+
+const getById = async id => {
+  // TODO: Too many database request
+  const getProject = await get()
+    .where({ id })
+    .first();
+  const actions = await actionDb
+    .getActions()
+    .where({ project_id: id })
+    .select(
+      "actions.id",
+      "actions.description",
+      "actions.notes",
+      "actions.completed"
+    );
+  return {
+    ...getProject,
+    actions
+  };
+};
+
+module.exports = {
+  insert,
+  getById
+};
