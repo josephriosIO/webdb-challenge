d0fbc08963761defabb0f6fa3247fecec3feee0c
josephriosio <blankworldwide@gmail.com>
2019-05-03 09:21:52 -0700

    fixed seeded database

--- data/seeds/001-projects.js
+++ data/seeds/001-projects.js
@@ -1,13 +1,25 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("projects")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("projects").insert([
+        {
+          name: "Magic.io",
+          description: "a magical site where magic is real",
+          completed: false
+        },
+        {
+          name: "databetes",
+          description: "a insulin app to get your BSL",
+          completed: false
+        },
+        {
+          name: "unnamed app",
+          description: "we dont know yet what it does",
+          completed: false
+        }
       ]);
     });
 };
